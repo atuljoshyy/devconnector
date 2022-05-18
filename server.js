071648b54e2c2a4173a0d49d3c1cf8c065672400
AtulJoshyy <atuljoshyy@gmail.com>
2022-05-19 01:09:00 +0530

    created routes

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.send('API running')
 })
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
+
 app.listen(port, () => {
     console.log(`app running on ${port}`);
 })
